be85edbe945149c33ebb5ebb08452d0bbe2f4240
Hamza S. <155594116+hmz-s@users.noreply.github.com>
2024-08-04 15:00:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ document.getElementById('toggle-dark-mode').addEventListener('click', function (
     document.body.classList.toggle('dark-mode');
 });
 
+document.getElementById('toggle-pink-mode').addEventListener('click', function () {
+    document.body.classList.toggle('pink-mode');
+});
 
